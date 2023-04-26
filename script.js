2f45ef43cecfd9a97151515b386b199904e88591
Sebastian <sebassha@stud.ntnu.no>
2023-04-26 13:22:42 +0200

    Tried working with GSAP library

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const treeContainer = document.querySelector(".tree-container");
 const trees = document.querySelectorAll(".tree");
 const bulldozers = document.querySelectorAll(".bulldozer");
+const bulldozerLeft = document.querySelector(".bulldozer-left");
+const bulldozerRight = document.querySelector(".bulldozer-right");
 
 const observer = new IntersectionObserver(
   (entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         // Tree is in view, start the animation
-        bulldozers.forEach((bulldozer) => {
-          bulldozer.classList.add("animate");
-          console.log(observer)
+        gsap.to(bulldozerLeft, {
+          duration: 2,
+          x: "50%",
+          ease: "power1.out",
+        });
+
+        gsap.to(bulldozerRight, {
+          duration: 2,
+          x: "50%",
+          ease: "power1.out",
+          delay: 1,
         });
       } else {
         // Tree is out of view, stop the animation
@@ -26,4 +36,3 @@ const observer = new IntersectionObserver(
 trees.forEach((tree) => {
   observer.observe(tree);
 });
-
