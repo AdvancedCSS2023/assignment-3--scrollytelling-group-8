22ffe92e98067ca25d92f992e8d22b3528ab1b44
Sebastian <sebassha@stud.ntnu.no>
2023-04-27 15:47:28 +0200

    Working on scene2

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const observer = new IntersectionObserver(
             duration: 6,
             x: "450%",
             repeat: -1,
-            ease: "Power0.easeOut",
+            ease: "Power0.easeInOut",
             onUpdate: function () {
               console.log("Bulldozer Left is animating");
             },
@@ -31,3 +31,40 @@ const observer = new IntersectionObserver(
 // Observe the bulldozer container
 observer.observe(bulldozerContainer);
 console.log(observer);
+
+// for scene2
+const plowContainer = document.querySelector('.plow-container');
+const triggerPoint = document.querySelector('.trigger-point');
+const wheatTrail = plowContainer.querySelector('.wheat-trail');
+
+const tl = new TimelineMax({
+  paused: true,
+  onComplete: function() {
+    this.pause();
+  }
+});
+
+tl.to(plowContainer, 3, {
+  x: '0',
+  repeat: -1,
+  ease: "Power1.easeInOut",
+  onUpdate: function () {
+    console.log("Tractor is moving");
+  },
+})
+.to(wheatTrail, 3, {
+  backgroundPosition: '100% 0',
+  ease: "Power1.easeInOut"
+}, '-=3');
+
+const detection = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      tl.play();
+    }
+  });
+}, {
+  threshold: 0.5
+});
+
+detection.observe(plowContainer);
