e4fd3ba029455682b6b649ce627151a5ff5882b7
Sebastian <sebassha@stud.ntnu.no>
2023-04-27 16:21:22 +0200

    Continue to work on scene2

--- script.js
+++ script.js
@@ -33,38 +33,29 @@ observer.observe(bulldozerContainer);
 console.log(observer);
 
 // for scene2
-const plowContainer = document.querySelector('.plow-container');
-const triggerPoint = document.querySelector('.trigger-point');
-const wheatTrail = plowContainer.querySelector('.wheat-trail');
+const plowContainer = document.querySelector(".plow-container");
+const tractor = document.querySelector(".tractor");
 
-const tl = new TimelineMax({
-  paused: true,
-  onComplete: function() {
-    this.pause();
-  }
-});
-
-tl.to(plowContainer, 3, {
-  x: '0',
-  repeat: -1,
-  ease: "Power1.easeInOut",
-  onUpdate: function () {
-    console.log("Tractor is moving");
+const detection = new IntersectionObserver(
+  (entries, detection) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        gsap.to(tractor, {
+          duration: 3,
+          x: "-250%",
+          repeat: -1,
+          ease: "Power0.easeInOut",
+          onUpdate: function () {
+            console.log("Tractor is moving");
+          },
+        });
+      } else {
+        // Tractor is out of view, stop the animation
+        gsap.killTweensOf(tractor);
+      }
+    });
   },
-})
-.to(wheatTrail, 3, {
-  backgroundPosition: '100% 0',
-  ease: "Power1.easeInOut"
-}, '-=3');
-
-const detection = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      tl.play();
-    }
-  });
-}, {
-  threshold: 0.5
-});
+  { threshold: 0.5 }
+);
 
 detection.observe(plowContainer);
