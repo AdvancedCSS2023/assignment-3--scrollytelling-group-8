5dc96575d370074b74a12447c827c078e9e5e1df
Sebastian <sebassha@stud.ntnu.no>
2023-05-12 11:26:28 +0200

    Starting from scratch

--- script.js
+++ script.js
@@ -1,59 +1,39 @@
-const bulldozerContainer = document.querySelector(".section__bulldozer-container");
-const bulldozers = document.querySelectorAll(".section__image--bulldozer");
+// Get the elements you want to animate
+const excavatorsImage = document.querySelector('.excavators__image');
+const excavatorsText = document.querySelector('.excavators__text');
+const cuttingImage = document.querySelector('.cutting__image');
+const cuttingText = document.querySelector('.cutting__text');
+const agricultureImage = document.querySelector('.agriculture__image');
+const agricultureText = document.querySelector('.agriculture__text');
+const positiveImage = document.querySelector('.positive__image');
+const positiveText = document.querySelector('.positive__text');
 
-const observer = new IntersectionObserver(
-  (entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        // Bulldozer container is in view, start the animation
-        bulldozers.forEach((bulldozer) => {
-          gsap.to(bulldozer, {
-            duration: 6,
-            x: "450%",
-            repeat: -1,
-            ease: "Power0.easeInOut",
-            onUpdate: function () {
-              console.log("Bulldozer Left is animating");
-            },
-          });
-        });
-      } else {
-        // Bulldozer container is out of view, stop the animation
-        bulldozers.forEach((bulldozer) => {
-          gsap.killTweensOf(bulldozer);
-        });
-      }
-    });
-  },
-  { threshold: 0.5 }
-);
+// Define the animation triggers based on the scroll position
+const excavatorsTrigger = excavatorsImage.offsetTop - window.innerHeight / 2;
+const cuttingTrigger = cuttingImage.offsetTop - window.innerHeight / 2;
+const agricultureTrigger = agricultureImage.offsetTop - window.innerHeight / 2;
+const positiveTrigger = positiveImage.offsetTop - window.innerHeight / 2;
 
-// Observe the bulldozer container
-observer.observe(bulldozerContainer);
-
-// for scene2
-const plowContainer = document.querySelector(".section__tractor-container");
-const tractor = document.querySelector(".section__image--tractor");
-
-const detection = new IntersectionObserver(
-  (entries, detection) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        gsap.to(tractor, {
-          duration: 5,
-          x: "-250%",
-          ease: "Power0.easeInOut",
-          onUpdate: function () {
-            console.log("Tractor is moving");
-          },
-        });
-      } else {
-        // Tractor is out of view, stop the animation
-        gsap.killTweensOf(tractor);
-      }
-    });
-  },
-  { threshold: 0.5 }
-);
-
-detection.observe(plowContainer);
+// Add a scroll event listener to trigger the animations
+window.addEventListener('scroll', () => {
+  // Check if the user has scrolled past the triggers for each section
+  if (window.pageYOffset >= excavatorsTrigger) {
+    excavatorsImage.classList.add('animate');
+    excavatorsText.classList.add('animate');
+  }
+  
+  if (window.pageYOffset >= cuttingTrigger) {
+    cuttingImage.classList.add('animate');
+    cuttingText.classList.add('animate');
+  }
+  
+  if (window.pageYOffset >= agricultureTrigger) {
+    agricultureImage.classList.add('animate');
+    agricultureText.classList.add('animate');
+  }
+  
+  if (window.pageYOffset >= positiveTrigger) {
+    positiveImage.classList.add('animate');
+    positiveText.classList.add('animate');
+  }
+});
